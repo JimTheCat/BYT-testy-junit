e088e52e43934d03a4c258b9a6de992f01da61cf
Jimmy <klosu10@gmail.com>
2024-01-05 22:21:32 +0100

    final version of account test

--- test/b_Money/AccountTest.java
+++ test/b_Money/AccountTest.java
@@ -22,23 +22,81 @@ public class AccountTest {
 		SweBank.deposit("Alice", new Money(1000000, SEK));
 	}
 
+	/**
+	 * Test add and remove timed payment method from Account
+	 * @throws AccountDoesNotExistException
+	 */
 	@Test
 	public void testAddRemoveTimedPayment() {
-		fail("Write test case here");
+		testAccount.addTimedPayment("1", 2, 2, new Money(100, SEK), SweBank, "Alice");
+		testAccount.addTimedPayment("2", 2, 2, new Money(100, SEK), SweBank, "Alice");
+		testAccount.addTimedPayment("3", 2, 2, new Money(100, SEK), SweBank, "Alice");
+		testAccount.addTimedPayment("4", 2, 2, new Money(100, SEK), SweBank, "Alice");
+		testAccount.addTimedPayment("5", 2, 2, new Money(100, SEK), SweBank, "Alice");
+
+		assertTrue("Timed payment does not exist", testAccount.timedPaymentExists("1"));
+		assertTrue("Timed payment does not exist", testAccount.timedPaymentExists("2"));
+		assertTrue("Timed payment does not exist", testAccount.timedPaymentExists("3"));
+		assertTrue("Timed payment does not exist", testAccount.timedPaymentExists("4"));
+		assertTrue("Timed payment does not exist", testAccount.timedPaymentExists("5"));
+
+		testAccount.removeTimedPayment("1");
+		testAccount.removeTimedPayment("2");
+		testAccount.removeTimedPayment("3");
+		testAccount.removeTimedPayment("4");
+		testAccount.removeTimedPayment("5");
+
+		assertFalse("Timed payment exists", testAccount.timedPaymentExists("1"));
+		assertFalse("Timed payment exists", testAccount.timedPaymentExists("2"));
+		assertFalse("Timed payment exists", testAccount.timedPaymentExists("3"));
+		assertFalse("Timed payment exists", testAccount.timedPaymentExists("4"));
+		assertFalse("Timed payment exists", testAccount.timedPaymentExists("5"));
 	}
 
+	/**
+	 * Test timed payment method from Account
+	 * @throws AccountDoesNotExistException
+	 */
 	@Test
 	public void testTimedPayment() throws AccountDoesNotExistException {
-		fail("Write test case here");
+		testAccount.addTimedPayment("1", 2, 2, new Money(100, SEK), SweBank, "Alice");
+		testAccount.addTimedPayment("2", 2, 2, new Money(100, SEK), SweBank, "Alice");
+		testAccount.addTimedPayment("3", 2, 2, new Money(100, SEK), SweBank, "Alice");
+		testAccount.addTimedPayment("4", 2, 2, new Money(100, SEK), SweBank, "Alice");
+		testAccount.addTimedPayment("5", 2, 2, new Money(100, SEK), SweBank, "Alice");
+
+		testAccount.tick();
+		testAccount.tick();
+		testAccount.tick();
+
+		assertEquals("Balance of account is not correct", 9999500, testAccount.getBalance().getAmount(), 0);
+		assertEquals("Balance of account is not correct", 1000500, SweBank.getBalance("Alice"), 0);
+
+		testAccount.tick();
+		testAccount.tick();
+		testAccount.tick();
+
+		assertEquals("Balance of account is not correct", 9999000, testAccount.getBalance().getAmount(), 0);
+		assertEquals("Balance of account is not correct", 1001000, SweBank.getBalance("Alice"), 0);
 	}
 
+	/**
+	 * Test deposit method from Account
+	 */
 	@Test
 	public void testAddWithdraw() {
-		fail("Write test case here");
+		testAccount.deposit(new Money(100, SEK));
+		assertEquals("Balance of account is not correct", 10000100, testAccount.getBalance().getAmount(), 0);
+
+		testAccount.withdraw(new Money(100, SEK));
+		assertEquals("Balance of account is not correct", 10000000, testAccount.getBalance().getAmount(), 0);
 	}
 
+	/**
+	 * Test get balance method from Account
+	 */
 	@Test
 	public void testGetBalance() {
-		fail("Write test case here");
+		assertEquals("Balance of account is not correct", 10000000, testAccount.getBalance().getAmount(), 0);
 	}
 }
